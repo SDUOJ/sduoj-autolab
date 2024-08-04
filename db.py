1d5eeb1b5974032e52c5fae1d8bd3d9385a08ca1
Hariko <13585134+tekzo-hariko@user.noreply.gitee.com>
2024-08-04 10:18:12 +0800

    修改录屏数据库，删除v_id，使用token作为主码

--- db.py
+++ db.py
@@ -353,8 +353,8 @@ class ScreenRecord(Base):
     # 录屏表，用于记录录屏信息
     __tablename__ = "screen_record"
 
-    # 视频id
-    v_id = Column(INTEGER, primary_key=True, nullable=False, autoincrement=True)
+    # 视频唯一凭证
+    token = Column(VARCHAR(50), nullable=False, primary_key=True, unique=True)  # 假设token应该是唯一的
 
     # 业务类型 业务ID
     bs_type = Column(INTEGER, nullable=False)
@@ -365,9 +365,6 @@ class ScreenRecord(Base):
 
     # 用户id
     u_id = Column(INTEGER, nullable=False)
-
-    # 视频唯一凭证
-    token = Column(VARCHAR(50), nullable=False, unique=True)  # 假设token应该是唯一的
 
     # 创建时间 最后修改时间
     start_time = Column(DATETIME, nullable=False)
