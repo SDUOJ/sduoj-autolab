821da0a48c75e96d22a4d95760f0663be3d587ec
纆冢大帅比 <120900893+mozhong123@users.noreply.github.com>
2024-08-04 14:06:32 +0800

    Update const.py

--- const.py
+++ const.py
@@ -1,8 +1,8 @@
-mysql_link_demo = 'mysql+pymysql://root:123456@localhost:3306/sduoj'  # mysql demo
+mysql_link_demo = 'mysql+pymysql://sduoj:sduoj@127.0.0.1:3306/sduoj'  # mysql demo
 redis_link_demo = 'redis://:@127.0.0.1:6379/0'
-Mysql_user = 'root'  # username
-Mysql_pass = '123456'  # password
-Mysql_addr = 'localhost:3306'  # mysql的address
+Mysql_user = 'sduoj'  # username
+Mysql_pass = 'sduoj'  # password
+Mysql_addr = '127.0.0.1:3306'  # mysql的address
 Mysql_db = 'sduoj'  # 数据库名称
 NACOS_addr = "127.0.0.1:8848"  # nacos的address
 NACOS_namespace = "dev"  # 命名空间，必须是dev
