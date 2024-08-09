1ae843ae1fc949510f736f7c46563f09a97caf8b
hz <11954175+sgrhz@user.noreply.gitee.com>
2024-08-09 11:10:03 +0800

    完成了签到部分的所有api

--- db.py
+++ db.py
@@ -422,7 +422,7 @@ class ojSign(Base):
 class ojSignUser(Base):
     __tablename__ = "oj_sign_user"
     # 学生签到id
-    sg_u_id = Column(INTEGER, primary_key=True, autoincrement=True, comment="学生签到id")
+    sg_u_id = Column(INTEGER, primary_key=True, autoincrement=1, comment="学生签到id")
 
     # 座位号
     seat_id = Column(INTEGER, unique=True, nullable=False, comment="座位号")
@@ -443,56 +443,10 @@ class ojSignUser(Base):
     sg_user_message = Column(LONGTEXT, nullable=True, comment="请假信息")
 
     # 是否通过审批
-    # 1 通过  0 未通过  none 审批中
+    # 1 通过  0 未通过  none 审批中  2 无请假
     sg_absence_pass = Column(INTEGER, nullable=True, comment="审批信息：1 通过  0 未通过  none 审批中")
 
 
-class OjSign(Base):
-    __tablename__ = "oj_sign"
-    sg_id = Column(INTEGER, primary_key=True, autoincrement=True, comment="签到id")
-    # 签到模式
-    mode = Column(INTEGER, nullable=False,comment="签到模式")
-    # 用户组id
-    group_id = Column(INTEGER, nullable=False,comment="用户组Id")
-    # 管理组id
-    m_group_id = Column(INTEGER, nullable=False,comment="管理组id")
-    # 创造时间
-    u_gmt_create =  Column(DATETIME, nullable=False, server_default=func.now(),comment="创建时间")
-    # 修改时间
-    u_gmt_modified = Column(DATETIME, nullable=False,comment="最后修改时间")
-    # 签到标签
-    title = Column(VARCHAR(63), nullable=False,comment="签到标签")
-    # 开始时间
-    start_time = Column(DATETIME, nullable=False,comment="签到开始时间")
-    # 结束时间
-    end_time = Column(DATETIME, nullable=False,comment="签到结束时间")
-    # 是否指定座位
-    # 1 绑定  0 未绑定
-    seat_bind = Column(INTEGER, nullable=False,comment="是否绑定座位：1 绑定  0 未绑定")
-    # 名单id
-    usl_id = Column(INTEGER, nullable=False,comment="名单id")
-
-
-# 用户签到表
-class OjSignUser(Base):
-    __tablename__ = "oj_sign_user"
-    # 学生签到id
-    sg_u_id = Column(INTEGER, primary_key=True, autoincrement=True, comment="学生签到id")
-    # 座位号
-    seat_id = Column(INTEGER, unique=True, nullable=False, comment="座位号")
-    # 学生用户名
-    username = Column(VARCHAR(63), unique=True, nullable=False, comment="学生用户名")
-    # 签到id
-    sg_id = Column(INTEGER, ForeignKey("oj_sign.sg_id"), nullable=False,index=True,comment="签到id")
-    # 签到时间
-    sg_time = Column(DATETIME, nullable=False,comment="签到时间")
-    # 签到唯一凭证
-    token = Column(VARCHAR(63), nullable=False, unique=True, comment="签到唯一凭证")
-    # 请假信息
-    sg_user_message = Column(LONGTEXT, nullable=True,comment="请假信息")
-    # 是否通过审批
-    # 1 通过  0 未通过  none 审批中
-    sg_absence_pass = Column(INTEGER, nullable=True,comment="审批信息：1 通过  0 未通过  none 审批中")
 
 from const import Mysql_addr, Mysql_user, Mysql_pass, Mysql_db
 
--- ser/sign_in_record.py
+++ ser/sign_in_record.py
@@ -1,9 +1,50 @@
+from datetime import datetime
+from http.client import HTTPException
 from typing import List, Optional, Union
 
 from pydantic import BaseModel
 from sqlalchemy.orm import session
-
 from db import ojClass, dbSession
+
+import os
+import uuid
+
+
+# 学生签到信息
+class signInType(BaseModel):
+    sg_u_id: int
+    username: str
+    sg_id: int
+    seat_id: int
+    sg_user_message: str = None
+
+
+class signType(BaseModel):
+    #签到信息
+    mode: int
+    group_id: int
+    m_group_id: int
+    title: str
+    start_time: datetime
+    end_time: datetime
+    seat_bind: int
+    usl_id: int = None
+
+
+class userSignIn(BaseModel):
+    username: str
+
+
+class signEditType(BaseModel):
+    mode: int = None
+    group_id: int = None
+    m_group_id: int = None
+    title: str = None
+    start_time: datetime = None
+    end_time: datetime = None
+    seat_bind: int = None
+    usl_id: int = None
+
 
 
 class submitLeaveInfoType(BaseModel):
@@ -16,3 +57,69 @@ class checkLeaveInfoType(BaseModel):
     # 审批用户提交的请假信息
     sg_u_id: int
     sg_absence_pass: int = None
+
+
+def sign_create(data: signType):
+    if data.mode is None:
+        raise HTTPException(status_code=400, detail="签到模式未填写")
+    elif data.mode not in range(0, 4):
+        raise HTTPException(status_code=400, detail="签到模式的输入不合法")
+    elif not data.m_group_id:
+        raise HTTPException(status_code=400, detail="管理组未填写")
+    elif not data.start_time or not data.end_time:
+        raise HTTPException(status_code=400, detail="开始或结束时间未填写")
+    elif not data.seat_bind:
+        raise HTTPException(status_code=400, detail="绑定信息未填写")
+
+    Now = datetime.now()
+    data={
+        "mode": data.mode,
+        "group_id": data.m_group_id,
+        "m_group_id": data.m_group_id,
+        "u_gmt_create": Now,
+        "u_gmt_modified": Now,
+        "title": data.title,
+        "start_time": data.start_time,
+        "end_time": data.end_time,
+        "seat_bind": data.seat_bind,
+        "usl_id": 1
+    }
+    return data
+
+
+def sign_edit(data: signEditType):
+    if data.mode not in [0, 1, 2, 3]:
+        raise HTTPException(status=400, detail="签到模式输入不合法")
+    Now = datetime.now()
+    data = {
+        "mode": data.mode,
+        "group_id": data.m_group_id,
+        "m_group_id": data.m_group_id,
+        "title": data.title,
+        "start_time": data.start_time,
+        "end_time": data.end_time,
+        "u_gmt_modified":Now,
+        "seat_bind": data.seat_bind,
+        "usl_id": data.usl_id
+    }
+    return data
+
+
+def checkIn(data: signInType):
+    Now = datetime.now()
+    Token = uuid.uuid4().hex
+    sg_absence_pass = None
+    #处理请假信息  # 1 通过  0 未通过  none 审批中  2 无请假
+    if data.sg_user_message is None:
+        sg_absence_pass = 2
+    data = {
+        "sg_u_id": data.sg_u_id,
+        "username": data.username,
+        "sg_id": data.sg_id,
+        "sg_time": Now,
+        "seat_id": data.seat_id,
+        "token": Token,
+        "sg_user_message": data.sg_user_message,
+        "sg_absence_pass": sg_absence_pass
+    }
+    return data
